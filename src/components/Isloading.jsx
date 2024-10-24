4d2298ece0fd221f871669a654646d408affb3e1
Eric_PC_FIXE <edelhaye93@gmail.com>
2024-10-24 19:39:44 +0200

    hehe

--- src/components/Isloading.jsx
+++ src/components/Isloading.jsx
@@ -5,7 +5,7 @@ export default function Isloading() {
   return (
     <div className='flex w-[24rem] h-[3rem]   justify-around items-center'>
         <div class="loader"></div>
-        <p className='text-sm font-semibold ml-[-2rem] animate-pulse'>This may take up to 90 seconds if server is asleep...</p>
+        <p className='text-sm font-semibold ml-[-1.5rem] animate-pulse'>This may take up to 90 seconds if server is asleep...</p>
     </div>
   )
 }
